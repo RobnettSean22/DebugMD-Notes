b90110b4aae8e78d2965c26b17a35a81dae803d9
RobnettSean22 <robnettsean22@gmail.com>
2019-09-22 14:58:03 -0700

    3rd commit

--- debugin.js
+++ debugin.js
@@ -1,1 +1,16 @@
 // syntax error/something is not defined and it should be: always check spelling and marks such as () {} etc
+
+
+
+//Varibilie specificts
+/*******************************************************
+ *if a symbol(@#$%^& etc) starts a var name (Uncaught SyntaxError: Invalid or unexpected token) 
+ * 
+ */ 
+
+ //if/else statments
+ /****************************************************** 
+  * puting return infront of true or false 
+  * */ 
+
+
